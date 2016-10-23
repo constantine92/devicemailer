0a76080cea2cb1fe6817676ea1033b0cae42f93a
Gabriel Su <gabrielsumiren@gmail.com>
2016-10-23 14:59:13 +0000

    added scaffold crud articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023053421) do
+ActiveRecord::Schema.define(version: 20161023072208) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",    null: false
