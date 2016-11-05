b706b80945de8d67d4afc51a5a2559360561b8ac
Gabriel Su <gabrielsumiren@gmail.com>
2016-11-05 21:34:54 +0000

    set up validation

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
-    default to: "info@gabriel.com"
-    def contact_email(name, email, message)
+default to: "info@chesnowitz.com"
+     def contact_email(name, email, message)
         @name = name
         @email = email
         @message = message
-        mail(from: email, subject: 'GabrielSu.com Contact Form Message')
+        mail(from: email, subject: 'Chesnowtiz.com Contact Form Message')
     end
 end
